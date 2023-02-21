a828b6975300a758be05d7b31f2f4a63889f9664
Ninad Badekar <112540181+Phineas8@users.noreply.github.com>
2023-02-21 15:18:18 +0530

    Update lmao.java

--- lmao.java
+++ lmao.java
@@ -1,7 +1,6 @@
 public class lmao{
     public static void main(String args[]){
         System.out.println("hello");
-        System.out.println("Where are youu??");
-        System.out.println("Still in your heart");
+
     }
 }
